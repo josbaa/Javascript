1e39840757a061c91f2c00c4874dc49f8122276f
Johannes Könönen <Johannes.Kononen@student.lab.fi>
2024-03-14 10:19:22 +0200

    tentti

--- js/tentti.js
+++ js/tentti.js
@@ -1,15 +1,29 @@
 
-  function laskeSumma() {
-    var pizzaHinta = parseFloat(document.getElementById("pizza").selectedOptions[0].getAttribute("data-hinta"));
-    var extraHinta = parseFloat(document.getElementById("extrat").value);
-    var alennus = document.getElementById("kantaAsiakas").checked ? 0.1 : 0;
-    var summa = (pizzaHinta + extraHinta) * (1 - alennus);
-    document.getElementById("summa").textContent = summa.toFixed(2);
-  }
 
-  function nollaaTilaus() {
-    document.getElementById("pizza").selectedIndex = 0;
-    document.getElementById("extrat").value = 0;
-    document.getElementById("kantaAsiakas").checked = false;
+function laskeSumma() {
+  var pizzaHinta = parseFloat(
+    document
+      .getElementById("pizza")
+      .selectedOptions[0].getAttribute("data-hinta")
+  );
+  var extraHinta = parseFloat(document.getElementById("extrat").value);
+  var alennus = document.getElementById("kantaAsiakas").checked ? 0.1 : 0;
+  var summa = (pizzaHinta + extraHinta) * (1 - alennus);
+  document.getElementById("summa").textContent = summa.toFixed(2);
+}
+
+
+   function lisaaExtra(hinta) {
+    extratSumma += hinta;
     laskeSumma();
   }
+
+$("#tilaaBtn").click(function () {
+  $("#pizza").val(0);
+  $("#extrat").val(0);
+  $("#kantaAsiakas").prop("checked", false);
+  laskeSumma();
+});
+
+
+laskeSumma();
